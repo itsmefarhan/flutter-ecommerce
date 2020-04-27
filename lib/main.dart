58dc4c87e1f6a87c1dec5fa03a4a6d7163ec6bad
Farhan Farooq <farhan.farooq@live.com>
2020-04-27 15:40:56 +0000

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_arena/screens/products_overview.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProductsOverview(),
+    );
+  }
+}
